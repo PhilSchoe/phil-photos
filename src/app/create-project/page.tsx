9aedef261c148a378ae83587caad854de13146d1
PhilSchoe <p.schoemacker@gmx.de>
2024-03-07 19:46:54 +0100

    Add image to project layout area and add borders

--- src/app/create-project/page.tsx
+++ src/app/create-project/page.tsx
@@ -1,13 +1,23 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function createProject() {
   return (
     <div className={styles.container}>
-      <div className={styles.projectLayout}>Image Placement</div>
+      <div className={styles.projectLayout}>
+        <Image
+          width={300}
+          height={300}
+          alt="Placeholder Image"
+          src="/vercel.svg"
+        />
+      </div>
 
       <div className={styles.uploadArea}>
         <div>Project Title: </div>
         <input type="text" />
+        <div>Select Images:</div>
+        <input type="file" accept="image/*" />
       </div>
     </div>
   );
