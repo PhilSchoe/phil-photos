84da2fe29fca5effd3c3bdad6b049925db2b1c7e
PhilSchoe <p.schoemacker@gmx.de>
2024-03-07 20:23:43 +0100

    Show preview for selected image

--- src/app/create-project/page.tsx
+++ src/app/create-project/page.tsx
@@ -1,23 +1,36 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
 
 export default function createProject() {
+  const [imageUrl, setImageUrl] = useState<string>("/vercel.svg");
+
+  const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) {
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setImageUrl(fileReader.result as string);
+    };
+
+    fileReader.readAsDataURL(event.target.files[0]);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.projectLayout}>
-        <Image
-          width={300}
-          height={300}
-          alt="Placeholder Image"
-          src="/vercel.svg"
-        />
+        <Image width={300} height={300} alt="Image" src={imageUrl} />
       </div>
 
       <div className={styles.uploadArea}>
         <div>Project Title: </div>
         <input type="text" />
         <div>Select Images:</div>
-        <input type="file" accept="image/*" />
+        <input type="file" accept="image/*" onChange={onFileChange} />
       </div>
     </div>
   );
