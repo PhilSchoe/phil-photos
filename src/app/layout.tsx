96393802ab7a9c2297b30eda3fceba1df95b1595
PhilSchoe <p.schoemacker@gmx.de>
2024-03-29 16:33:46 +0100

    Refactor project structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import styles from "./layout.module.css";
-import Footer from "./ui/footer";
-import Header from "./ui/header";
+import Footer from "../ui/footer";
+import Header from "../ui/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
