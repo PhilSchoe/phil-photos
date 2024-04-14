78d2c975c77694872a688533c2a4d397cf393ebd
PhilSchoe <p.schoemacker@gmx.de>
2024-04-14 14:40:34 +0200

    Set up creation of a MinIO default bucket

--- src/db/minio.ts
+++ src/db/minio.ts
@@ -20,6 +20,25 @@ if (process.env.NODE_ENV === "production") {
 } else {
   if (!global.minioClient) {
     global.minioClient = new Minio.Client(config);
+
+    const minioDefaultBucket = process.env.MINIO_DEFAULT_BUCKET as string;
+
+    if (!minioDefaultBucket) {
+      throw new Error("Missing MINIO default bucket!");
+    }
+
+    global.minioClient
+      .bucketExists(minioDefaultBucket)
+      .then((exists: boolean) => {
+        if (exists) {
+          console.log(
+            `Default MINIO Bucket ${minioDefaultBucket} is existing!`
+          );
+        } else {
+          console.log(`Creating default MINIO Bucket ${minioDefaultBucket}!`);
+          global.minioClient.makeBucket(minioDefaultBucket, "eu-central-1");
+        }
+      });
   }
   minioClient = global.minioClient;
 }
