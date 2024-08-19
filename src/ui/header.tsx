fca9932e80bbd49c12e1dcfb384ddb414ee09c71
PhilSchoe <p.schoemacker@gmx.de>
2024-08-19 21:31:16 +0200

    Refactor to header scss and center title

--- src/ui/header.tsx
+++ src/ui/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import ButtonGroup from "./components/button/buton-group";
 import Button from "./components/button/button";
-import styles from "./header.module.css";
+import styles from "./header.module.scss";
 import { useRouter } from "next/navigation";
 
 export default function Header() {
@@ -13,12 +13,16 @@ export default function Header() {
   ];
 
   return (
-    <div className={`${styles.headerContent} ${styles.headerBorder}`}>
-      <div>
-        <ButtonGroup buttonProps={buttonProps} />
+    <div className={`${styles.Header} ${styles.headerBorder}`}>
+      <div className={styles["Header--left"]}>
+        <div>
+          <ButtonGroup buttonProps={buttonProps} />
+        </div>
       </div>
-      <div className={styles.title}>Phil-Photos</div>
-      <div>
+
+      <div className={styles["Header--title"]}>Phil-Photos</div>
+
+      <div className={styles["Header--right"]}>
         <Button
           title="Sign In"
           isActive={false}
